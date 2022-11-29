af1a95b770e1e6c0ac3d0ec8d1f7caec8de2e92d
afranci2 <108776240+afranci2@users.noreply.github.com>
2022-11-29 11:34:36 -0500

    initial commit

---
+++ dall-e/src/App.jsx
@@ -0,0 +1,51 @@
+import { Configuration, OpenAIApi } from "openai";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const[prompt,setPrompt] = useState("")
+  const[result,setResult] = useState("")
+  const configuration = new Configuration({ 
+    apiKey: import.meta.env.VITE_Open_AI_Key,
+  });
+  const openai = new OpenAIApi(configuration);
+
+
+  const generateImage = async () => {
+    const res = await openai.createImage({
+      prompt: prompt,
+      n:1,
+      size: "512x512",
+    });
+
+    setResult(res.data.data[0].url);
+  };
+
+
+  return (
+    <div className="app-main">
+      <>
+        <h2>Generate an Image using Open AI API</h2>
+
+        <textarea
+          className="app-input"
+          placeholder="Search Bears with Paint Brushes the Starry Night, painted by Vincent Van Gogh.."
+          onChange={(e) => setPrompt(e.target.value)}
+          rows="10"
+          cols="40"
+        />
+        <button onClick={generateImage}>Generate an Image</button>
+      </>
+      {result.length > 0 ? (
+          <img className="result-image" src={result} alt="result" />
+        ) : (
+          <></>
+        )}
+      <div className="example-text">
+        <p1> Lorem ipsum dolor sit, amet consectetur adipisicing elit. A fuga asperiores facilis tempore odio quod saepe quidem nisi culpa mollitia quibusdam earum, ipsum labore sapiente veritatis totam nihil illum. Aspernatur!</p1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
